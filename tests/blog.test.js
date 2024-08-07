ce51adc535feca14a42c0962c0f56aac17237eef
navdiya-nikunj <nikunj@cybersync-technologies.com>
2024-08-07 12:27:56 +0530

    Exercise 4.4

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -7,3 +7,29 @@ test('dummy returns one', () => {
     const result = list_helper.dummy(blogs);
     assert.equal(result, 1);
 })
+
+describe('Total Likes', () => {
+
+    test('bigger list and calculated right', () => {
+        const blogs = [
+            {
+                likes: 30
+            },
+            {
+                likes: 40
+            },
+            {
+                likes: 40
+            }
+        ]
+        assert.equal(list_helper.totalLikes(blogs), 110);
+    })
+
+    test("zero likes for empty list", () => {
+        assert.equal(list_helper.totalLikes([]), 0);
+    })
+
+    test("One element list", () => {
+        assert.equal(list_helper.totalLikes([{ likes: 14 }]), 14);
+    })
+})
