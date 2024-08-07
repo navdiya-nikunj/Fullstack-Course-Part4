ce97ed23886404c48f7f07ad900658a9a555143e
navdiya-nikunj <nikunj@cybersync-technologies.com>
2024-08-07 12:14:18 +0530

    Exercise 4.3

---
+++ tests/blog.test.js
@@ -0,0 +1,9 @@
+const { test, describe } = require('node:test');
+const assert = require('node:assert')
+const list_helper = require('../utils/list_helper');
+
+test('dummy returns one', () => {
+    const blogs = [];
+    const result = list_helper.dummy(blogs);
+    assert.equal(result, 1);
+})
